2e45e0d592c63f3689b45d7cc3d946919bd07254
perinazzoo <gabrielperinazo@gmail.com>
2020-02-02 00:24:36 -0300

    first-commit

---
+++ backend/src/config/database.js
@@ -0,0 +1,12 @@
+module.exports = {
+  dialect: 'postgres',
+  host: 'localhost',
+  username: 'postgres',
+  password: 'senha123',
+  database: 'fastfeet',
+  define: {
+    timestamps: true,
+    underscored: true,
+    underscoredAll: true,
+  },
+};
---
+++ backend/src/database/migrations/20200202030801-create-recipients.js
@@ -0,0 +1,9 @@
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable('users', { id: Sequelize.INTEGER });
+  },
+
+  down: queryInterface => {
+    return queryInterface.dropTable('recipients');
+  },
+};
