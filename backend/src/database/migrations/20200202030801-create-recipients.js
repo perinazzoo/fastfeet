abcfe1a8720cfa2af91d66460770ffb0361067d9
perinazzoo <gabrielperinazo@gmail.com>
2020-02-02 01:16:09 -0300

    migration and seeds added

--- backend/src/database/migrations/20200202030801-create-recipients.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  up: (queryInterface, Sequelize) => {
-    return queryInterface.createTable('users', { id: Sequelize.INTEGER });
-  },
-
-  down: queryInterface => {
-    return queryInterface.dropTable('recipients');
-  },
-};
